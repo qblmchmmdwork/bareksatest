7ae754c8cbab9735eafde710e3544511b0105daf
Mochammad Iqbal <qblmchmmd.dev@gmail.com>
2021-03-31 05:34:03 +0700

    redesign ReksaDanaModel

--- app/src/main/java/com/bareksa/bareksatest/model/ReksaDana.kt
+++ app/src/main/java/com/bareksa/bareksatest/model/ReksaDana.kt
@@ -1,14 +1,18 @@
 package com.bareksa.bareksatest.model
 
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
 import java.util.*
 
 data class ReksaDana(
     val id: String,
+    @ColorRes val color: Int,
+    @DrawableRes val image: Int,
     val nama: String,
     val jenis: JenisReksaDana,
     val imbalHasil: ImbalHasil,
-    val danaKelolaan: Long,
-    val minPembelian: Int,
+    val danaKelolaan: String,
+    val minPembelian: String,
     val jangkaWaktuTahun: Int,
     val tingkatResiko: TingkatResiko,
     val tanggalPeluncuran: Date
--- app/src/sharedTest/java/com.bareksa.bareksatest/Dummies.kt
+++ app/src/sharedTest/java/com.bareksa.bareksatest/Dummies.kt
@@ -7,18 +7,26 @@ import com.bareksa.bareksatest.model.TingkatResiko
 import java.util.*
 
 fun generateReksaDanaDummies(size: Int) = List(size) {
+    val index = it+1
+    val (color, drawable) = when (it % 3) {
+        0 -> R.color.green_50 to R.drawable.bni
+        1 -> R.color.violet_50 to R.drawable.ciptadana
+        else -> R.color.navy_50 to R.drawable.ascend
+    }
     ReksaDana(
-        id = "$it",
-        nama = "Reksa Dana $it",
-        jenis = JenisReksaDana.Saham,
+        id = "$index",
+        color = color,
+        image = drawable,
+        nama = "Reksa Dana $index",
+        jenis = JenisReksaDana.values()[index % JenisReksaDana.values().size],
         imbalHasil = ImbalHasil(
-            nilai = 5.50,
-            perTahun = 5
+            nilai = 5.50 * index,
+            perTahun = index
         ),
-        danaKelolaan = 3_640_000_000,
-        minPembelian = 1_000_000,
-        jangkaWaktuTahun = 5,
+        danaKelolaan = "$index Miliar",
+        minPembelian = "$index Juta",
+        jangkaWaktuTahun = 5 * index,
         tingkatResiko = TingkatResiko.Tinggi,
-        tanggalPeluncuran = Date()
+        tanggalPeluncuran = Date(500000000L * index)
     )
 }
